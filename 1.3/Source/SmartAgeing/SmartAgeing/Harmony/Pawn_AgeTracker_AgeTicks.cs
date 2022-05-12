d766ccd2b30ba04b86e6e5085ccc209ebdea2c75
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-05-12 08:58:36 +0200

    Initial commit

---
+++ 1.3/Source/SmartAgeing/SmartAgeing/Harmony/Pawn_AgeTracker_AgeTicks.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Reflection.Emit;
+
+
+
+namespace SmartAgeing
+{
+
+
+    [HarmonyPatch(typeof(Pawn_AgeTracker))]
+    [HarmonyPatch("AgeTick")]
+    public static class SmartAgeing_Pawn_AgeTracker_AgeTick_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+
+
+            var codes = instructions.ToList();
+         
+            for (var i = 0; i < codes.Count; i++)
+            {
+                
+
+                if (codes[i].opcode == OpCodes.Ldc_I4 && (int)(codes[i].operand) == 3600000)
+                {
+                   
+                    yield return new CodeInstruction(OpCodes.Ldc_I4,360);
+                }
+                
+                else
+                {
+                    yield return codes[i];
+                }
+
+
+            }
+
+
+        }
+    }
+}

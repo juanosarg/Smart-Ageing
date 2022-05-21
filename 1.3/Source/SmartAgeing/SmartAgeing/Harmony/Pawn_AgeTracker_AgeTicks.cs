45617efc28a631e15c30903adc45e1c38e68b50d
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-05-21 09:30:05 +0200

    v1.2

--- 1.3/Source/SmartAgeing/SmartAgeing/Harmony/Pawn_AgeTracker_AgeTicks.cs
+++ 1.3/Source/SmartAgeing/SmartAgeing/Harmony/Pawn_AgeTracker_AgeTicks.cs
@@ -16,14 +16,36 @@ namespace SmartAgeing
         [HarmonyPostfix]
         static void DoBirthdays(Pawn_AgeTracker __instance,Pawn ___pawn)
         {
-            if(SmartAgeing_Settings.affectAnimals || (!SmartAgeing_Settings.affectAnimals && !___pawn.def.race.Animal))
+            if (!___pawn.def.race.Animal)
             {
                 if (Find.TickManager.TicksGame % (3600000 / SmartAgeing_Settings.ageingRate) == 0)
                 {
-                    MethodInfo birthday = AccessTools.Method(typeof(Pawn_AgeTracker), "BirthdayBiological");
-                    birthday.Invoke(__instance, null);
+                    if (SmartAgeing_Settings.applyOldAgeDiseases)
+                    {
+                        MethodInfo birthday = AccessTools.Method(typeof(Pawn_AgeTracker), "BirthdayBiological");
+                        birthday.Invoke(__instance, null);
+                    }
+                    MethodInfo growth = AccessTools.Method(typeof(Pawn_AgeTracker), "CalculateGrowth");
+                    growth.Invoke(__instance, new object[] {(int)(240* SmartAgeing_Settings.ageingRate) });
                 }
             }
+            else
+            {
+                if (SmartAgeing_Settings.affectAnimals)
+                {
+
+                    if (Find.TickManager.TicksGame % (3600000 / SmartAgeing_Settings.animalAgeingRate) == 0)
+                    {
+                        if (SmartAgeing_Settings.applyOldAgeDiseases)
+                        {
+                            MethodInfo birthday = AccessTools.Method(typeof(Pawn_AgeTracker), "BirthdayBiological");
+                            birthday.Invoke(__instance, null);
+                        }
+                        MethodInfo growth = AccessTools.Method(typeof(Pawn_AgeTracker), "CalculateGrowth");
+                        growth.Invoke(__instance, new object[] { (int)(240 * SmartAgeing_Settings.ageingRate) });
+                    }
+                }
+            }       
         }
     }
 
@@ -34,11 +56,23 @@ namespace SmartAgeing
         [HarmonyPrefix]
         static void AgeTickIncrease(ref long ___ageBiologicalTicksInt, ref long ___birthAbsTicksInt, Pawn ___pawn)
         {
-            if (SmartAgeing_Settings.affectAnimals || (!SmartAgeing_Settings.affectAnimals && !___pawn.def.race.Animal))
+
+
+            if (!___pawn.def.race.Animal)
             {
                 ___ageBiologicalTicksInt += SmartAgeing_Settings.ageingRate - 1;
                 ___birthAbsTicksInt -= (SmartAgeing_Settings.ageingRate - 1);
             }
+            else
+            {
+                if (SmartAgeing_Settings.affectAnimals)
+                {
+
+                    ___ageBiologicalTicksInt += SmartAgeing_Settings.animalAgeingRate - 1;
+                    ___birthAbsTicksInt -= (SmartAgeing_Settings.animalAgeingRate - 1);
+                }
+            }
+
          } 
     }
 }
--- 1.3/Source/SmartAgeing/SmartAgeing/Options/SmartAgeing_Settings.cs
+++ 1.3/Source/SmartAgeing/SmartAgeing/Options/SmartAgeing_Settings.cs
@@ -18,7 +18,12 @@ namespace SmartAgeing
         public const int baseAgeingRate = 1;
         public const int maxAgeingRate = 1000;
 
+        public static int animalAgeingRate = baseAnimalAgeingRate;
+        public const int baseAnimalAgeingRate = 1;
+        public const int maxAnimalAgeingRate = 1000;
+
         public static bool affectAnimals = true;
+        public static bool applyOldAgeDiseases = true;
 
 
 
@@ -28,7 +33,10 @@ namespace SmartAgeing
         {
             base.ExposeData();
             Scribe_Values.Look(ref ageingRate, "ageingRate", baseAgeingRate, true);
+            Scribe_Values.Look(ref animalAgeingRate, "animalAgeingRate", baseAnimalAgeingRate, true);
+
             Scribe_Values.Look(ref affectAnimals, "affectAnimals", true, true);
+            Scribe_Values.Look(ref applyOldAgeDiseases, "applyOldAgeDiseases", true, true);
 
 
 
@@ -42,6 +50,8 @@ namespace SmartAgeing
 
             ls2.Begin(inRect);
             ls2.CheckboxLabeled("SA_affectAnimals".Translate(), ref affectAnimals, "SA_affectAnimalsTooltip".Translate());
+            ls2.CheckboxLabeled("SA_applyOldAgeDiseases".Translate(), ref applyOldAgeDiseases, "SA_applyOldAgeDiseasesTooltip".Translate());
+
 
             var ageingRateLabel = ls2.LabelPlusButton("SA_ageingRate".Translate() + ": " + ageingRate, "SA_ageingRateTooltip".Translate());
             ageingRate = (int)Math.Round(ls2.Slider(ageingRate, baseAgeingRate, maxAgeingRate), 0);
@@ -50,8 +60,15 @@ namespace SmartAgeing
             {
                 ageingRate = baseAgeingRate;
             }
+            var animalAgeingRateLabel = ls2.LabelPlusButton("SA_animalAgeingRate".Translate() + ": " + animalAgeingRate, "SA_animalAgeingRateTooltip".Translate());
+            animalAgeingRate = (int)Math.Round(ls2.Slider(animalAgeingRate, baseAnimalAgeingRate, maxAnimalAgeingRate), 0);
 
-          
+            if (ls2.Settings_Button("SA_Reset".Translate(), new Rect(0f, animalAgeingRateLabel.position.y + 35, 250f, 29f)))
+            {
+                animalAgeingRate = baseAnimalAgeingRate;
+            }
+
+
 
             ls2.End();
         }
